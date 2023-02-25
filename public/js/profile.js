359f73e929b36839f84d43fa4453769594036a60
Rachel L Wildberger <rachellwildberger@Rachels-MacBook-Pro.local>
2023-02-24 14:31:08 -1000

    fixed bug in models/User

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,47 +1,47 @@
-const newFormHandler = async (event) => {
-    event.preventDefault();
+// const newFormHandler = async (event) => {
+//     event.preventDefault();
   
-    const post_title = document.querySelector('#post-title').value.trim();
-    const post_desc = document.querySelector('#post-desc').value.trim();
+//     const post_title = document.querySelector('#post-title').value.trim();
+//     const post_desc = document.querySelector('#post-desc').value.trim();
   
-    if (post_title && post_desc) {
-      const response = await fetch(`/api/post`, {
-        method: 'POST',
-        body: JSON.stringify({ post_title, post_desc }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+//     if (post_title && post_desc) {
+//       const response = await fetch(`/api/post`, {
+//         method: 'POST',
+//         body: JSON.stringify({ post_title, post_desc }),
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//       });
   
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to create post');
-      }
-    }
-  };
+//       if (response.ok) {
+//         document.location.replace('/profile');
+//       } else {
+//         alert('Failed to create post');
+//       }
+//     }
+//   };
   
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
+//   const delButtonHandler = async (event) => {
+//     if (event.target.hasAttribute('data-id')) {
+//       const id = event.target.getAttribute('data-id');
   
-      const response = await fetch(`/api/post/${id}`, {
-        method: 'DELETE',
-      });
+//       const response = await fetch(`/api/post/${id}`, {
+//         method: 'DELETE',
+//       });
   
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to delete post');
-      }
-    }
-  };
+//       if (response.ok) {
+//         document.location.replace('/profile');
+//       } else {
+//         alert('Failed to delete post');
+//       }
+//     }
+//   };
   
-  document
-    .querySelector('.new-post-form')
-    .addEventListener('submit', newFormHandler);
+//   document
+//     .querySelector('.new-post-form')
+//     .addEventListener('submit', newFormHandler);
   
-  document
-    .querySelector('.post-list')
-    .addEventListener('click', delButtonHandler);
+//   document
+//     .querySelector('.post-list')
+//     .addEventListener('click', delButtonHandler);
   
